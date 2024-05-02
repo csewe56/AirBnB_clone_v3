71a2cdf0e5d8aa3a965db13b9dc024d6bef5bcdc
csewe56 <csewe56@gmail.com>
2024-05-02 02:36:32 -0700

    web_static files & installed html files

--- console.py
+++ console.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-""" console """
+''' consol'''
 
 import cmd
 from datetime import datetime
 import models
-from models.amenity import Amenity
+from models.place import Place
 from models.base_model import BaseModel
 from models.city import City
-from models.place import Place
+from models.amenity import Amenity
 from models.review import Review
 from models.state import State
 from models.user import User
 import shlex  # for splitting the line along spaces except in double quotes
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {'Place': Place, 'BaseModel': BaseModel, 'City': City,
+           'Amenity': Amenity, 'Review': Review, 'State': State, 'User': User}
 
 
 class HBNBCommand(cmd.Cmd):
-    """ HBNH console """
+    '''HBNH console '''
     prompt = '(hbnb) '
 
     def do_EOF(self, arg):
